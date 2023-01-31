57934f67608553a875a1d03be4b63a017c8d65e3
Mohamed Mahrous <eng.mahrous17@gmail.com>
2023-01-31 22:07:20 +0200

    upload contact

--- js/global.js
+++ js/global.js
@@ -1,7 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Fixed Header on Scroll
+  const Header = document.getElementById("fixedHeader");
+
+  if (Header) {
+    window.addEventListener("scroll", function () {
+      if (this.scrollY > Header.offsetHeight) {
+        !Header.classList.contains("fixed") && Header.classList.add("fixed");
+      } else {
+        Header.classList.contains("fixed") && Header.classList.remove("fixed");
+      }
+    });
+  }
+
+  // Clear Search Button
+  const searchField = document.getElementById("navSearch"),
+    clearSearchBtn = document.getElementById("clearSearch");
+
+  // Show / Hide Clear Button
+  if (searchField) {
+    searchField.addEventListener("keyup", function () {
+      if (this.value) {
+        !clearSearchBtn.classList.contains("show") &&
+          clearSearchBtn.classList.add("show");
+      } else {
+        clearSearchBtn.classList.contains("show") &&
+          clearSearchBtn.classList.remove("show");
+      }
+    });
+  }
+
+  // Click on Clear Button
+  if (clearSearchBtn) {
+    clearSearchBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      searchField.value = "";
+      this.classList.remove("show");
+    });
+  }
+
+  // Show Mobile Search
+  const mobileSearchAction = [
+    document.querySelector(".mobile-search"),
+    document.querySelector(".search-return"),
+  ];
+
+  if (mobileSearchAction[0]) {
+    mobileSearchAction.forEach((el) => {
+      el.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const parentEl = this.closest(".search-form");
+        parentEl && parentEl.classList.toggle("show");
+      });
+    });
+  }
+
+  // Trigger Lazy Load
   const lazyImages = document.querySelectorAll(".lazy");
   if (lazyImages.length !== 0) {
-    // Trigger Lazy Load
     const lazyLoadInstance = new LazyLoad();
   }
 
